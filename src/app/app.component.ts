29f8bc666296bad3ad515dfc7fb0d5d34b7af37a
kamleeshdugade <kamleshdpatil777@gmail.com>
2021-12-11 00:43:53 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { Component, ViewChild, ElementRef  } from '@angular/core';
+import jsPDF from 'jspdf';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfmake from 'html-to-pdfmake';
 export class AppComponent {
-  title = 'Bill-generator';
+  @ViewChild('pdfTable') pdfTable: ElementRef;
+  //PDF genrate button click function
+  public downloadAsPDF() {
+    const doc = new jsPDF();
+    //get table html
+    const pdfTable = this.pdfTable.nativeElement;
+    //html to pdf format
+    var html = htmlToPdfmake(pdfTable.innerHTML);
+
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).open();
+
+
+  }
+
+
+
 }
